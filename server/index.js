8ea274a0765d0454e6f6cfc0fcc61a6d3baf6b9e
pavel_kalinichenko_cr <pavel.kalinichenko.cr@gmail.com>
2018-09-28 18:07:44 +0300

    Implemented server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
 
 app.use(express.static(__dirname + '/../dist/real-chat-app'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
 });
